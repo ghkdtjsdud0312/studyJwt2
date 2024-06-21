315eaa86d3cd9ffa2904296c8de2ba4ee5ed1988
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-06-21 16:29:03 +0900

    6/21 CustomUserDetails 내용추가

--- src/main/java/jwtLogin/jwt_login/dto/CustomUserDetails.java
+++ src/main/java/jwtLogin/jwt_login/dto/CustomUserDetails.java
@@ -5,11 +5,12 @@ import jwtLogin.jwt_login.entity.UserEntity;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class CustomUserDetails implements UserDetails {
 
-    // 초기화하기
+    // 초기화 하기
     private final UserEntity userEntity;
 
     public CustomUserDetails(UserEntity userEntity) {
@@ -17,38 +18,59 @@ public class CustomUserDetails implements UserDetails {
     }
 
 
+    // 권한 값
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+
+            @Override
+            public String getAuthority() {
+                return userEntity.getRole();
+            }
+        });
+        return collection;
     }
 
+    // 비밀번호 값
     @Override
     public String getPassword() {
-        return null;
+
+        return userEntity.getPassword();
     }
 
+    // 사용자 아이디 값
     @Override
     public String getUsername() {
-        return null;
+        return userEntity.getUserName();
     }
 
+    // 계정의 만료 여부
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        // true는 설정이 막히지 않았다라는 의미
+        return true;
     }
 
+    // 계정의 잠김 여부
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        // true는 설정이 막히지 않았다라는 의미
+        return true;
     }
 
+    // 계정 비밀번호 만료 상태
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        // true는 설정이 막히지 않았다라는 의미
+        return true;
     }
 
+    // 계정의 존재 유무
     @Override
     public boolean isEnabled() {
-        return false;
+        // true는 설정이 막히지 않았다라는 의미
+        return true;
     }
 }
